2b9a559a7013a6f05bf9e6da10239334812590dc
Jason Coleman <jayemcee121@gmail.com>
2023-03-14 11:43:35 -0400

    Getting list of Robots, Working on POST method

--- Controllers/RobotDistanceController.cs
+++ Controllers/RobotDistanceController.cs
@@ -1,3 +1,4 @@
+using Dapper;
 using Microsoft.AspNetCore.Mvc;
 using RobotDistanceApi.Models;
 using System.Text.Json;
@@ -5,11 +6,12 @@ using System.Text.Json;
 namespace RobotDistanceApi.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("robots")]
     public class RobotDistanceController : ControllerBase
     {
         private readonly ILogger<RobotDistanceController> _logger;
         private HttpClient client = new();
+        
 
         public RobotDistanceController(ILogger<RobotDistanceController> logger)
         {
@@ -22,12 +24,21 @@ namespace RobotDistanceApi.Controllers
             await using Stream stream = await client.GetStreamAsync("https://60c8ed887dafc90017ffbd56.mockapi.io/robots");
             var robots = await JsonSerializer.DeserializeAsync<List<Robot>>(stream);
 
-            foreach (var robot in robots)
+            foreach (Robot robot in robots)
             {
-                Console.WriteLine("RobotID: " + robot.robotId + " BatteryLevel: " + robot.batteryLevel);
+                Console.WriteLine("RobotID: " + robot.robotId + " BatteryLevel: " + robot.batteryLevel + " Coordinates: " + "x: " + robot.x + " " + "y: " + robot.y);   
             }
 
-            yield return Enumerable.Range(1, 5).Select(index => new Robot()).ToArray();
+            yield return robots.Select(robot => robot).ToArray();
+        }
+
+        [HttpPost]
+        [Route("closest")]
+        [Produces("application/json")]
+        public IActionResult Post([FromBody] Load load)
+        {
+            // Logic to create new Employee
+            return Ok(load);
         }
     }
 }
