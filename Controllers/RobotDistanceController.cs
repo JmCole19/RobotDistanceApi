46addc0c228657e62f02faee38a6b52c1cba2296
Jason Coleman <jayemcee121@gmail.com>
2023-03-14 15:12:48 -0400

    Code Cleanup

--- Controllers/RobotDistanceController.cs
+++ Controllers/RobotDistanceController.cs
@@ -20,41 +20,21 @@ namespace RobotDistanceApi.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "RobotList")]
-        public async IAsyncEnumerable<Robot[]> Get()
-        {
-            await using Stream stream = await client.GetStreamAsync("https://60c8ed887dafc90017ffbd56.mockapi.io/robots");
-            var robots = await JsonSerializer.DeserializeAsync<List<Robot>>(stream);
-
-            foreach (Robot robot in robots)
-            {
-                Console.WriteLine("RobotID: " + robot.robotId + " BatteryLevel: " + robot.batteryLevel + " Coordinates: " + "x: " + robot.x + " " + "y: " + robot.y);
-            }
-
-            yield return robots.Select(robot => robot).ToArray();
-        }
-
         [HttpPost]
         [Route("closest")]
         [Produces("application/json")]
         [Consumes("application/json")]
         public async IAsyncEnumerable<Robot> Post(Load load)
         {
-            Console.WriteLine(load);
             await using Stream stream = await client.GetStreamAsync("https://60c8ed887dafc90017ffbd56.mockapi.io/robots");
             var robots = await JsonSerializer.DeserializeAsync<List<Robot>>(stream);
-
-            Console.WriteLine(robots);
 
             foreach (Robot robot in robots)
             {
                 double distanceToGoal = Math.Sqrt(Math.Pow(robot.x - load.x, 2) + Math.Pow(robot.y - load.y, 2));
                 robot.distanceToGoal = distanceToGoal;
-                Console.WriteLine(robot);
             }
             Robot closestRobot = robots.Find(robot => robot.distanceToGoal == robots.Min(robot => robot.distanceToGoal));
-
-            Console.WriteLine(closestRobot);
 
             yield return closestRobot;
         }
